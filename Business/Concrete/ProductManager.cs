fb4a2c9dc258e246f258cbd73e22f5e30fe09d8c
KOPAZAR 2 <KOPAZAR 2@KP-2>
2022-12-02 01:32:16 +0300

    Proje dosyası ekle.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+        public List<Product> GetAll()
+        {
+            return _productDal.GetAll();
+            
+
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+
+
+
+ProductManager productManager = new ProductManager(new InMemoryProductDal());
+
+foreach (var product in productManager.GetAll())
+{
+    Console.WriteLine(product.ProductName);
+}
+
